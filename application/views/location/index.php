1885d2998dbfa1edcb3fa10c401783650523f44d
rahul <rahullodhi3636@gmail.com>
2022-10-19 23:13:17 +0530

    refecto:format the code file issue #1

--- application/views/location/index.php
+++ application/views/location/index.php
@@ -6,11 +6,11 @@ $this->load->view('templates/header');
         <span id="success-msg"></span>
     </div>
 </div>
- <form class="dynamic-dependent-frm" id="dynamic-dependent-frm">
+<form class="dynamic-dependent-frm" id="dynamic-dependent-frm">
     <div class="row">
         <div class="col-lg-12">
             <div class="form-group">
-                <select title="Select Country" name="regcountry" class="form-control" id="country-name">      
+                <select title="Select Country" name="regcountry" class="form-control" id="country-name">
                     <option value="">Select Country</option>
                     <?php
                     foreach ($geCountries as $key => $element) {
@@ -24,7 +24,7 @@ $this->load->view('templates/header');
     <div class="row">
         <div class="col-lg-12">
             <div class="form-group">
-                <select title="Select State" name="state_name" class="form-control" id="state-name">      
+                <select title="Select State" name="state_name" class="form-control" id="state-name">
                     <option value="">Select State</option>
                 </select>
             </div>
@@ -33,7 +33,7 @@ $this->load->view('templates/header');
     <div class="row">
         <div class="col-lg-12">
             <div class="form-group">
-                <select title="Select City" name="city_name" class="form-control" id="city-name">      
+                <select title="Select City" name="city_name" class="form-control" id="city-name">
                     <option value="">Select City</option>
                 </select>
             </div>
